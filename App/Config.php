e7bbaffc30040cf729dac7650155526d62ab4601
Hrishikesh Vaze <hrishikesh.vaze01@gmail.com>
2021-01-19 14:23:17 +0530

    balanced 1

--- App/Config.php
+++ App/Config.php
@@ -2,13 +2,13 @@
 
 	$landing_controller = 'Home';
 
-	$base_url = "http://localhost/dc/webhook/";
-	// $base_url = "https://worldchat-hrishi.herokuapp.com/";
+	// $base_url = "http://localhost/dc/webhook/";
+	$base_url = "https://worldchat-hrishi.herokuapp.com/";
 
 	$DATABASE = array(
 		'status'    =>false,
 		'hostname'  =>'localhost',
-		'username'  =>'root',
+		'username'  =>'root', 
 		'password'  =>'',
 		'dbname'    =>'test'
 	);
