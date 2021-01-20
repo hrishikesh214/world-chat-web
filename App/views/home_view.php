f4542fa504b2a1576e4d80b97cbb62d19cef58f5
Hrishikesh Vaze <hrishikesh.vaze01@gmail.com>
2021-01-20 12:20:06 +0530

    d1

--- App/views/home_view.php
+++ App/views/home_view.php
@@ -12,6 +12,29 @@
 		body, html{
 			padding: 0 0 0 0;
 			margin: 0 0 0 0;
+			background-image: linear-gradient(45deg, #edb62b , #c9eb50);
+			background-color: rgba(0, 0, 0, 0.3);
+			animation: changetc 10s infinite linear;
+		}
+		.inner-container::-webkit-scrollbar{
+			background-color: rgba(0,0,0,0);
+			width:6px;
+		}
+		.inner-container::-webkit-scrollbar-thumb{
+			background-color: black;
+		}
+		.inner-container::-webkit-scrollbar-button{
+			background-color: rgba(0,0,0,0.7);
+		}
+		.inner-container::-ms-scrollbar{
+			background-color: rgba(0,0,0,0.4);
+			width:6px;
+		}
+		.inner-container::-ms-scrollbar-thumb{
+			background-color: black;
+		}
+		.inner-container::-ms-scrollbar-button{
+			background-color: rgba(0,0,0,0.7);
 		}
 		.foot-msgbar{
 			background-image: linear-gradient(45deg, #1ddef0 , #1ff09d);
@@ -59,8 +82,8 @@
 			font-size: 1.2rem;
 		}
 		.navbar{
-			/*background-image: linear-gradient(to right, #2ad1cc , #18f079);*/
-			background-color: black;
+			background-image: linear-gradient(to right, #2ad1cc , #18f079);
+			/*background-color: black;*/
 			height: 10vh;
 			width: 100vw;
 			display: flex;
@@ -85,16 +108,17 @@
 			font-size: 2rem;
 			margin-right: 10vw;
 			background-color: transparent;
-			text-decoration: overline;
+			/*text-decoration: overline;*/
+			text-decoration-style: dotted;
 		}
 		.hele{
 			color: transparent;
-			text-decoration: overline;
+			/*text-decoration: overline;*/
 			display: inline;
 			width: 100%;
 			height: 100%;
 			background-color: red;
-			background-image: linear-gradient(90deg, #e63d05, #a6f01d);
+			background-image: linear-gradient(90deg, #edb62b, #c9eb50);
 			background-size: 100%;
 			background-repeat: repeat;
 			-webkit-background-clip: text;
@@ -102,6 +126,8 @@
 			-moz-background-clip: text;
 			-moz-text-fill-color: transparent;
 			animation: changetc 7s infinite linear;
+			-webkit-text-stroke-width: 0.5px;
+  			-webkit-text-stroke-color: black;
 
 		}
 		@keyframes changetc{
@@ -139,18 +165,49 @@
 			position: absolute;
 			transform: translate(-50%, -50%);
 			/*background-color: rgba(1, 1, 1, 0.4);*/
+			overflow-x: wrap;
+			overflow-y: scroll;
+			padding: 10px 10px 10px 10px;
+			display: flex;
+			flex-direction: column; 
+			border: 2px black solid;
+			border-radius: 5px 5px 5px 5px; 
 		}
 		.link{
 			cursor: pointer;
 			background-color: transparent;
 			font-size: 1rem;
 			position: relative;
-			border: 2px white solid;
+			border: 2px black solid;
 			border-radius: 5px;
-			color: white; 
+			color: black; 
 			padding: 6px 6px 6px 6px;
 
 		}
+		.in-msg{
+			/*background-color: rgba(1,1,1,0.5);*/
+			width: auto;
+			height: auto;
+			display: inline;
+			margin-bottom: 0.5%;
+			margin-top: 0.5%;
+			padding: 0.5% 0.5% 0.5% 0.5%;
+			display: flex;
+			flex-direction: column;
+			border: 2px black solid;
+			border-radius: 5px 5px 5px 5px;
+		}
+		.in-msg > .m-body{
+			margin-left: 2%;
+
+		}
+		.m-time{
+			font-size: 0.9rem;
+		}
+		.m-name{
+			text-decoration: underline;
+		}
+
 		@media only screen and (max-width: 480px){
 			.fmb{
 				left:0%;
@@ -166,6 +223,14 @@
 			}
 			.send-btn{
 				height: 5vh;
+			}
+			.container{
+				margin: 0 0 0 0;
+				padding: 0 0 0 0;
+			}
+			.inner-container{
+				margin: 0 0 0 0;
+				padding: 0 0 0 0;
 			}
 		}
 	</style>
@@ -185,7 +250,12 @@
 	</div>
 	<div class="container">
 		<div class="inner-container">
-			<?php //debug($_SESSION);?>
+			<?php for($i=0; $i<=10; $i++): ?>
+				<div class="in-msg">
+					<div class="m-header"><span class="m-avatar">A</span> | <span class="m-name">Hrishikesh</span> <span class="m-time">| 9.30 pm</span> </div>
+					<div class="m-body">This is sample msg</div>
+				</div>
+			<?php endfor ?>
 		</div>
 	</div>
 	<div class="foot-msgbar">
